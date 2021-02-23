c0445b0d13f00c42ce8ab1fec15c2d3cc0686d91
ajith7566 <ajith.devadiga@covalensedigital.com>
2021-02-23 15:05:03 +0530

    program process using fork()

---
+++ forkdemo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main(){
+	int pid = 0;
+	pid = fork();
+	if(pid == 0)
+		printf("Inside child process\n");
+	else
+		printf("Inside parent process\n");
+	
+	return 0;
+
+}
